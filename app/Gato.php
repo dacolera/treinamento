355707899f0107510618f8682e3ae11cc4d2fbce
Jurandir Dacol Junior <dacolera360@gmail.com>
2020-01-16 12:17:57 -0300

    [dacolera] broke gato

--- app/Gato.php
+++ app/Gato.php
@@ -5,6 +5,14 @@ namespace App;
 
 final class Gato extends Animal
 {
+    protected $rabo;
+
+    public function __construct($cor, $idade, $rabo)
+    {
+        parent::__construct($cor, $idade);
+        $this->rabo = $rabo;
+    }
+
     public function miar()
     {
         return 'miiiiiaaaauuuuu';
